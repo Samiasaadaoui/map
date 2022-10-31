8526d48e6a08e51b179fde49a77365ff9bc57654
unknown <samiasaadaooui@gmail.com>
2022-10-31 16:24:15 +0100

    google

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import react from "react"; 
+
+function app(){
+  return(
+    <div className="app">
+    
+    
+   <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=2034%20ezzahra%20tunis&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+   
+    <p> </p>
+
+    </div>
+
+  );
+}
+export default app;
